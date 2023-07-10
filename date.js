3c63a5140c2b1cbbe9d31ea2c199e396f3efa044
W A <wa@Willenss-MBP.lan>
2023-07-09 21:16:48 -0400

    first commit

---
+++ date.js
@@ -0,0 +1,12 @@
+
+exports.getDate = function() { 
+
+    let today = new Date();
+    
+        const options = {
+            weekday: "long",
+            day: "numeric",
+            month: "long"
+        };
+        return today.toLocaleDateString("en-US", options);
+    };
